69b50b33efc6d300331de58f5c0aabbb1824fa7c
dream-girl888 <87891649+dream-girl888@users.noreply.github.com>
2021-09-12 13:40:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
+console.log("Hello Winc Academy")
+
 let name = "dream-girl888";
 console.log(name);
 
-//leeftijd in getal en text
-let age = 30;
-console.log(age);
+// je kunt geen variabele aanmaken met de naam if, want dat is een gereserveerde trefwoord net als 'let'.
 
-let ageText = "dertig";
-console.log(ageText);
+//als number als in string optellen, let op bij string wel haakjes erbij
+let sum = 4 + 4;
+console.log("sum:", sum);
+let sumWithStrings = "4" + "4";
+console.log("sum with strings:", sumWithStrings);
+
 
 //sommetjes
-let y1 = 4;
-let x1 = y1 + 4;
-console.log(x1);
+let addition = 5 + 3;
+console.log("addition:", addition);
+let subtraction = 20 - 2;
+console.log("subtraction:", subtraction);
+let multiplication = 10 * 10;
+console.log("multiplication:", multiplication);
+let division = 9 / 3;
+console.log("division:", division);
+let modulus = 12 % 9;
+console.log("modulus:", modulus);
 
-let y2 = 4;
-let x2 = y2 - 4;
-console.log(x2);
 
-let y3 = 4;
-let x3 = y3 * 4;
-console.log(x3);
+//leeftijd in type getal en text
+//Maak een variabele met je leeftijd in cijfers (bijvoorbeeld 48), log* het type variabele naar de console met typeof (typeof operator)
+let age = 48;
+console.log(typeof age);
+//Verander nu het type van deze variable door je leeftijd ("48") uit te schrijven in "text", log het type naar de console met typeof .
+let ageAsString = "48";
+console.log(typeof ageAsString)
 
-let y4 = 4;
-let x4 = y4 / 4;
-console.log(x4);
-
-let y5 = 4;
-let x5 = y5 % 4;
-console.log(x5);
 
