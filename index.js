e6f1e421d16f9e1f41311980d63b451bb2e45c2b
dream-girl888 <87891649+dream-girl888@users.noreply.github.com>
2021-09-12 13:15:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let name = "dream-girl888";
+console.log(name);
+
+//leeftijd in getal en text
+let age = 30;
+console.log(age);
+
+let ageText = "dertig";
+console.log(ageText);
+
+//sommetjes
+let y1 = 4;
+let x1 = y1 + 4;
+console.log(x1);
+
+let y2 = 4;
+let x2 = y2 - 4;
+console.log(x2);
+
+let y3 = 4;
+let x3 = y3 * 4;
+console.log(x3);
+
+let y4 = 4;
+let x4 = y4 / 4;
+console.log(x4);
+
+let y5 = 4;
+let x5 = y5 % 4;
+console.log(x5);
+
